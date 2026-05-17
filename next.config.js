1bc004cfacd2940915aff8115181ac30a8642ba0
adnan53p <125865218+adnan53p@users.noreply.github.com>
2026-05-17 10:45:48 +0700

    Add image domains and redirect configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   images: {
     domains: ['images.unsplash.com', 'via.placeholder.com'],
     formats: ['image/avif', 'image/webp'],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'rumah-honda-motor.vercel.app',
+          },
+        ],
+        destination: 'https://www.rumahhondamotor.my.id/:path*',
+        permanent: true,
+      },
+    ]
   },
 }
 
